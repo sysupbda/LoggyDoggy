5aa8e5a84be4bd3e2957d7e652663e1ce3c07b22
Ben Dadsetan <ben@primary-directives.com>
2024-03-04 07:30:43 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using Serilog;
+
+
+
+var logger = new LoggerConfiguration()
+   .WriteTo.Console(outputTemplate: "[{ElasticApmTraceId} {ElasticApmTransactionId} {Message:lj} {NewLine}{Exception}")
+   .CreateLogger();
+
+
+
+Console.WriteLine("Hello, World!");
+var src = new ActivitySource("Test");
+using var activity1 = src.StartActivity(nameof(Sample), ActivityKind.Server);
+logger.Information("Hello, World!");
+
+
+
+Thread.Sleep(100);
+
+internal class Sample
+{
+}
